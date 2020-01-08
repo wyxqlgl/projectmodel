7551a5207204c0c0a81ebb0799b13919930992b2
wyxqlgl@sina.com <wyxqlgl@sina.com>
2020-01-08 13:23:31 +0800

    no message

---
+++ Equipment_testing/src/main/java/com/equipmenttesting/EquipmentTestingApplication.java
@@ -0,0 +1,20 @@
+package com.equipmenttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class EquipmentTestingApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EquipmentTestingApplication.class, args);
+        System.out.println("----------------------启动成功------------------------------------");
+    }
+}
---
+++ Equipment_testing/src/main/java/com/equipmenttesting/EquipmentTestingServletInitializer.java
@@ -0,0 +1,18 @@
+package com.equipmenttesting;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author ruoyi
+ */
+public class EquipmentTestingServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(EquipmentTestingApplication.class);
+    }
+}
